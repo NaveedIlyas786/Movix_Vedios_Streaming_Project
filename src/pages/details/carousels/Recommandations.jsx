0f075afa158fb35bb6ac4f595317c29019360369
NaveedIlyas <79637722+NaveedIlyas786@users.noreply.github.com>
2023-04-30 15:53:33 +0500

    Update Recommandations.jsx

--- src/pages/details/carousels/Recommandations.jsx
+++ src/pages/details/carousels/Recommandations.jsx
@@ -4,8 +4,9 @@ import Carousel from "../../../components/carousel/Carousel";
 import useFetch from "../../../hooks/useFetch";
 
 const Recommendation = ({ mediaType, id }) => {
-    const { data, loading, error } = useFetch(`/${mediaType}/${id}/similar`);
-    // const title = mediaType === "tv" ? "Recommendation TV Shows" : "Recommendation Movies";
+    const { data, loading, error } = useFetch(
+        `/${mediaType}/${id}/recommendations`
+    );
 
     return (
         <Carousel
